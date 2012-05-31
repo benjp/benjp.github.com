1c96058751f1901ef2d793cd945e2cf6aa274544
Benjamin Paillereau <bpaillereau@exoplatform.com>
2012-05-31 16:58:39 +0200

    Cleaning index.html

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$.getJSON('json/nav-collapse.json', function(data) {
+  var html = Mustache.to_html(navCollapseTpl, data);
+  $('#nav-collapse').html(html);
+}); 
+
+$.get('html/index.html', function(data) {
+  $('#main-container').html(data);
+});
+
+$.getJSON('json/sidebar-nav.json', function(data) {
+  var html = Mustache.to_html(sidebarNavTpl, data);
+  $('#sidebar-nav').html(html);
+});    
+
+$.getJSON('json/sidebar-categories.json', function(data) {
+  var html = Mustache.to_html(sidebarNavTpl, data);
+  $('#sidebar-categories').html(html);
+});    
