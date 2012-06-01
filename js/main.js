9be846d105090cc119357cdb3456742071a9b68e
Benjamin Paillereau <bpaillereau@exoplatform.com>
2012-06-01 14:17:42 +0200

    bookmark able, history compliant navigation

--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
-if (typeof String.prototype.startsWith != 'function') {
-  String.prototype.startsWith = function (str){
-    return this.indexOf(str) == 0;
-  };
-}
-
 $(document).ready(function () {
   $.getJSON('json/nav-collapse.json', function(data) {
     var html = Mustache.to_html(navCollapseTpl, data);
     $('#nav-collapse').html(html);
   }); 
   
-  $.get('html/index.html', function(data) {
-    $('#main-container').html(data);
-  });
+
+  $('#main-container').html('&nbsp;');
+
   
   $.getJSON('json/sidebar-nav.json', function(data) {
     var html = Mustache.to_html(sidebarNavTpl, data);
@@ -24,35 +18,42 @@ $(document).ready(function () {
     $('#sidebar-categories').html(html);
   }); 
 
+});
 
-  $.address.change(function(event){
-   $('.nav li').removeClass('active');
-    var anchor = event.value.substring(1);
-    if (anchor.startsWith('article')) {
-      $.get('contents/1.html', function(data) {
+
+
+if (typeof String.prototype.startsWith != 'function') {
+  String.prototype.startsWith = function (str){
+    return this.indexOf(str) == 0;
+  };
+}
+
+
+
+$.address.change(function(event){
+  $('.nav li').removeClass('active');
+  var anchor = event.value.substring(1);
+  if (anchor=="") anchor = "index";
+  if (anchor.startsWith('article')) {
+    $.get('contents/1.html', function(data) {
+      $('#main-container').html(data);
+    });      
+  }
+
+  if (anchor == 'aboutme' || anchor == 'contact' || anchor == 'index' ) {
+
+    $('#nav-'+anchor).addClass('active');
+
+    $('#main-container').fadeOut('fast', function() {
+      $.get('html/'+anchor+'.html', function(data) {
         $('#main-container').html(data);
-      });      
-    }
-
-    if (anchor == 'aboutme' || anchor == 'contact' || anchor == 'index' ) {
-
-      $('#nav-'+anchor).addClass('active');
-
-      $('#main-container').fadeOut('fast', function() {
-        $.get('html/'+anchor+'.html', function(data) {
-          $('#main-container').html(data);
-          $('#main-container').fadeIn('fast', function() {
-        
-          });  
-        });
+        $('#main-container').fadeIn('fast', function() {
       
-      });  
-      
-    }
+        });  
+      });
+    
+    });  
+    
+  }
 
-  });
-
-
-
-  
 });
