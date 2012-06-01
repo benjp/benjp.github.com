2f89fbbf869b109ee5b66e9a7a9d7757efa28ad6
Benjamin Paillereau <bpaillereau@exoplatform.com>
2012-06-01 12:32:48 +0200

    removing view logic from json articles

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
-$.getJSON('json/nav-collapse.json', function(data) {
-  var html = Mustache.to_html(navCollapseTpl, data);
-  $('#nav-collapse').html(html);
-}); 
+$(document).ready(function () {
+  $.getJSON('json/nav-collapse.json', function(data) {
+    var html = Mustache.to_html(navCollapseTpl, data);
+    $('#nav-collapse').html(html);
+  }); 
+  
+  $.get('html/index.html', function(data) {
+    $('#main-container').html(data);
+  });
+  
+  $.getJSON('json/sidebar-nav.json', function(data) {
+    var html = Mustache.to_html(sidebarNavTpl, data);
+    $('#sidebar-nav').html(html);
+  });    
+  
+  $.getJSON('json/sidebar-categories.json', function(data) {
+    var html = Mustache.to_html(sidebarNavTpl, data);
+    $('#sidebar-categories').html(html);
+  }); 
 
-$.get('html/index.html', function(data) {
-  $('#main-container').html(data);
+
+  
 });
-
-$.getJSON('json/sidebar-nav.json', function(data) {
-  var html = Mustache.to_html(sidebarNavTpl, data);
-  $('#sidebar-nav').html(html);
-});    
-
-$.getJSON('json/sidebar-categories.json', function(data) {
-  var html = Mustache.to_html(sidebarNavTpl, data);
-  $('#sidebar-categories').html(html);
-});    
 
--- js/mustache-templates.js
+++ js/mustache-templates.js
@@ -3,6 +3,3 @@ $.get('tpl/sidebar-nav.mustache', function(templ) { sidebarNavTpl = templ; });
 
 var navCollapseTpl;
 $.get('tpl/nav-collapse.mustache', function(templ) { navCollapseTpl = templ; });
-
-var indexTpl;
-$.get('tpl/index.mustache', function(templ) { indexTpl = templ; });
