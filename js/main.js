68e5b3bd8b0ddf6cf7a490f00fc5bf2eb0fcf19a
Benjamin Paillereau <bpaillereau@exoplatform.com>
2012-06-08 10:18:35 +0200

    cleaning js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   $('#main-container').html('&nbsp;');
 
   $.get('html/sidebar.html', function(data) {
-  	$('#sidebar-categories').html(data);
+    $('#sidebar-categories').html(data);
   });
     
 });
@@ -13,21 +13,21 @@ var curCat, curType;
 $.address.change(function(event){
   $('.nav li').removeClass('active');
   var anchor = event.value.substring(1);
-  if (anchor=="") anchor = "index";
+  if (anchor==="") anchor = "index";
   var url = 'html/index.html';
 
-  if (anchor.indexOf('article')==0) {   //eg. anchor.startsWith('article')
+  if (anchor.indexOf('article')===0) {   //eg. anchor.startsWith('article')
     var id = anchor.substring(anchor.indexOf("_")+1);
     url = 'contents/'+id+'.html';
-  } else if (anchor.indexOf('category')==0) {   //eg. anchor.startsWith('category')
+  } else if (anchor.indexOf('category')===0) {   //eg. anchor.startsWith('category')
     curCat = anchor.substring(anchor.indexOf("_")+1);
     curType = "category";
     url = 'html/archives.html';
-  } else if (anchor.indexOf('tag')==0) {   //eg. anchor.startsWith('tag')
+  } else if (anchor.indexOf('tag')===0) {   //eg. anchor.startsWith('tag')
     curCat = anchor.substring(anchor.indexOf("_")+1);
     curType = "tag";
     url = 'html/archives.html';
-  } else if (anchor == 'aboutme' || anchor == 'contact' || anchor == 'index' ) {
+  } else {
     $('#nav-'+anchor).addClass('active');
     url = 'html/'+anchor+'.html';
   }
@@ -37,9 +37,9 @@ $.address.change(function(event){
     $.get(url, function(data) {
       $('#main-container').html(data);
       $('#main-container').fadeIn(100, function() {
-      });  
-    });    
-  });  
+      });
+    });
+  });
 
   
 });
